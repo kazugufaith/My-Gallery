c84f5205ab1613999dcc8703d54b6510053d8562
kazugufaith <faithmugesia@gmail.com>
2019-06-20 08:48:00 +0300

    added name of the project username and password of the database in setting.py file

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -77,59 +77,59 @@ TEMPLATES = [
 WSGI_APPLICATION = 'Gallery.wsgi.application'
 
 
-# # Database
-# # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'gallery2',
-#         'USER': 'moringa',
-#         'PASSWORD': '1234',
-#     }
-# }
-
-
-# # Password validation
-# # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
-
-# AUTH_PASSWORD_VALIDATORS = [
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-#     },
-#     {
-#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-#     },
-# ]
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery2',
+        'USER': 'moringa',
+        'PASSWORD': '1234',
+    }
+}
 
 
-# # Internationalization
-# # https://docs.djangoproject.com/en/1.11/topics/i18n/
+# Password validation
+# https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
-# LANGUAGE_CODE = 'en-us'
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
 
-# TIME_ZONE = 'Africa/Nairobi'
 
-# USE_I18N = True
+# Internationalization
+# https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-# USE_L10N = True
+LANGUAGE_CODE = 'en-us'
 
-# USE_TZ = True
+TIME_ZONE = 'Africa/Nairobi'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
 
 
-# # Static files (CSS, JavaScript, Images)
-# # https://docs.djangoproject.com/en/1.11/howto/static-files/
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
