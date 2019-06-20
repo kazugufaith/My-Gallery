5a75f69f5195661546f37a4f6b0684bdaf10dcd0
kazugufaith <faithmugesia@gmail.com>
2019-06-20 09:08:37 +0300

    class editor and defined them in models.py file

--- personal/models.py
+++ personal/models.py
@@ -1,24 +1,24 @@
-# import datetime as dt
-# from django.db import models
+import datetime as dt
+from django.db import models
 
-# class Editor(models.Model):
-#     first_name = models.CharField(max_length =30)
-#     last_name = models.CharField(max_length =30)
-#     email = models.EmailField()
-#     phone_number = models.CharField(max_length = 10,blank =True)
+class Editor(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank =True)
 
 
-#     def __str__(self):
-#         return self.first_name
+    def __str__(self):
+        return self.first_name
 
-#     def save_editor(self):
-#         self.save()
+    def save_editor(self):
+        self.save()
 
-#     def delete_editor(self):
-#         self.delete()
+    def delete_editor(self):
+        self.delete()
 
-#     class Meta:
-#         ordering = ['first_name']
+    class Meta:
+        ordering = ['first_name']
 
 
 # class tags(models.Model):
