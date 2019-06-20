cb6bc3c486e457c348760dad9b20dd8f3a14ec7b
kazugufaith <faithmugesia@gmail.com>
2019-06-20 09:06:15 +0300

    registered image,editor and tags in admin.py file

--- personal/admin.py
+++ personal/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import Editor,Image,tags
 #Register your models here.
 
-# class ImageAdmin(admin.ModelAdmin):
-#     # filter_horizontal =('location',)
+class ImageAdmin(admin.ModelAdmin):
+    # filter_horizontal =('location',)
 
-#     admin.site.register(Editor)
-#     admin.site.register(Image)
-#     admin.site.register(tags)
+    admin.site.register(Editor)
+    admin.site.register(Image)
+    admin.site.register(tags)
