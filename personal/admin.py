a123dcf4ef8befd280dc28758978aff1378b68da
kazugufaith <faithmugesia@gmail.com>
2019-06-20 09:05:14 +0300

    imported images variables from models

--- personal/admin.py
+++ personal/admin.py
@@ -1,6 +1,6 @@
-# from django.contrib import admin
-# from .models import Editor,Image,tags
-# #Register your models here.
+from django.contrib import admin
+from .models import Editor,Image,tags
+#Register your models here.
 
 # class ImageAdmin(admin.ModelAdmin):
 #     # filter_horizontal =('location',)
